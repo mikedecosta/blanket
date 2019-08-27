d0c8b2e278956224851bb488575333938d056b65
Mike DeCosta <mike_decosta@yahoo.com>
2019-08-27 14:23:10 -0400

    Split off geocoder functions to geocoder.js

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
 'use strict';
 
-const APPCONFIG = require('./app-config');
 const express = require('express');
-const nodeGeocoder = require('node-geocoder');
+const geocoder = require('./geocoder');
 
 const app = express();
 app.use(express.json());
-
-const DISTANCE_UNIT = "miles";
-const geocoder = nodeGeocoder({
-    provider: 'google',
-    apiKey: APPCONFIG.GOOGLE_API_KEY
-});
 
 app.get('/', (request, response) => {
   response.status(200).send('Hello, local world!');
@@ -22,22 +15,20 @@ app.post('/locations', (request, response) => {
         throw new Error('request requires "locations" array not to be empty');
     }
 
-    geocoder.batchGeocode(request.body.locations, function(error, results) {
-        if(error) {
-            throw new Error('Request Error: ', error);
-        }
+    geocoder.batchGeocode(request.body.locations).then(function(geocodedLocations) {
+
         const apiResults = {
             accepted: [],
             rejected: []
         };
-        for(let i=0;i<results.length;i++) {
-            let target = results[i];
+        for(let i=0;i<geocodedLocations.length;i++) {
+            let target = geocodedLocations[i];
             if(target.error) {
                 apiResults.rejected.push(target);
                 continue;
             }
 
-            let { closestPoint, distance } = getClosestPoint(target, results);
+            let { closestPoint, distance } = geocoder.getClosestPoint(target, geocodedLocations);
             if(!closestPoint) {
                 apiResults.rejected.push(target.value[0].formattedAddress);
                 continue;
@@ -52,52 +43,6 @@ app.post('/locations', (request, response) => {
         response.end(JSON.stringify(apiResults));
     });
 });
-
-function calculateDistance(locationA, locationB, unit) {
-    const radiusLatitudeA = Math.PI * locationA.latitude/180;
-    const radiusLatitudeB = Math.PI * locationB.latitude/180;
-    const theta = locationA.longitude - locationB.longitude;
-    const radiusTheta = Math.PI * theta/180;
-
-    let distance = Math.sin(radiusLatitudeA) * Math.sin(radiusLatitudeB) + Math.cos(radiusLatitudeA) * Math.cos(radiusLatitudeB) * Math.cos(radiusTheta);
-    if (distance > 1) {
-        distance = 1;
-    }
-
-    distance = Math.acos(distance);
-    distance = distance * 180/Math.PI;
-    distance = distance * 60 * 1.1515;
-
-    if (unit=="km") { distance = dist * 1.609344 }
-
-    return distance;
-}
-
-function getClosestPoint(target, locations) {
-    if(target.error) {
-        return null;
-    }
-
-    let closestPoint;
-    let closestPointDistance;
-    for(let i=0;i<locations.length;i++) {
-        let loc = locations[i];
-        if (loc.error || loc.value[0].formattedAddress === target.value[0].formattedAddress) {
-            continue;
-        }
-
-        let distance = calculateDistance(target.value[0], loc.value[0], DISTANCE_UNIT);
-        if(!closestPoint || distance < closestPointDistance) {
-            closestPoint = loc;
-            closestPointDistance = distance;
-        }
-    }
-
-    return { 
-        'closestPoint': closestPoint, 
-        'distance': Math.round(closestPointDistance) + " " + DISTANCE_UNIT
-    };
-}
 
 if (module === require.main) {
   const server = app.listen(process.env.PORT || 8080, () => {
---
+++ geocoder.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const APPCONFIG = require('./app-config');
+const nodeGeocoder = require('node-geocoder');
+
+const DISTANCE_UNIT = "miles";
+const geocoder = nodeGeocoder({
+    provider: 'google',
+    apiKey: APPCONFIG.GOOGLE_API_KEY
+});
+
+module.exports.batchGeocode = function (locations) {
+    return geocoder.batchGeocode(locations, function(error, results) {
+        if(error) {
+            throw new Error('Request Error: ', error);
+        }
+
+        return results;
+    });
+}
+
+module.exports.getClosestPoint = function (target, locations) {
+    if(target.error) {
+        return null;
+    }
+
+    let closestPoint;
+    let closestPointDistance;
+    for(let i=0;i<locations.length;i++) {
+        let loc = locations[i];
+        if (loc.error || loc.value[0].formattedAddress === target.value[0].formattedAddress) {
+            continue;
+        }
+
+        let distance = calculateDistance(target.value[0], loc.value[0], DISTANCE_UNIT);
+        if(!closestPoint || distance < closestPointDistance) {
+            closestPoint = loc;
+            closestPointDistance = distance;
+        }
+    }
+
+    return { 
+        'closestPoint': closestPoint, 
+        'distance': Math.round(closestPointDistance) + " " + DISTANCE_UNIT
+    };
+}
+
+function calculateDistance(locationA, locationB, unit) {
+    const radiusLatitudeA = Math.PI * locationA.latitude/180;
+    const radiusLatitudeB = Math.PI * locationB.latitude/180;
+    const theta = locationA.longitude - locationB.longitude;
+    const radiusTheta = Math.PI * theta/180;
+
+    let distance = Math.sin(radiusLatitudeA) * Math.sin(radiusLatitudeB) + Math.cos(radiusLatitudeA) * Math.cos(radiusLatitudeB) * Math.cos(radiusTheta);
+    if (distance > 1) {
+        distance = 1;
+    }
+
+    distance = Math.acos(distance);
+    distance = distance * 180/Math.PI;
+    distance = distance * 60 * 1.1515;
+
+    if (unit=="km") { distance = dist * 1.609344 }
+
+    return distance;
+}
+
